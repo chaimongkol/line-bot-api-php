aeb6009c73f51101d3cd35d90775ea1eb714db44
Ging Para <nongeng52@gmail.com>
2019-07-12 14:58:27 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'B5K+LK728NyRrAJhByOZlHCwmiveLxwqs8zXrsZdPeQbXvm609xosboPJqiRzxICbuN1FqqiSnh+Nx4mx4OITNr8bTXIjFGrbB9bUtnCYH8FXpQ3GBoGlylhTNPMHToQ2uTVWLfNBWnYFq6AXzgIpgdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
